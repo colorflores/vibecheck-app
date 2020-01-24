72914f3485f15706ed4b7c18ffbc5a959bce2cc3
Omar <omar.garciaf247@gmail.com>
2020-01-24 14:15:37 -0500

    added more options to test navigations

--- components/TestNavigation.tsx
+++ components/TestNavigation.tsx
@@ -10,6 +10,26 @@ export default class TestNavigation extends React.Component {
           title="Landing Page"
           onPress={() => navigate('Landing')}
         />
+        <Button
+          title="Profile Page"
+          onPress={() => navigate('Profile')}
+        />
+        <Button
+          title="Profile Page"
+          onPress={() => navigate('Menu')}
+        />
+        <Button
+          title="Profile Page"
+          onPress={() => navigate('Vibecheck')}
+        />
+        <Button
+          title="Profile Page"
+          onPress={() => navigate('Vibe recommendation')}
+        />
+        <Button
+          title="Profile Page"
+          onPress={() => navigate('Loading')}
+        />
       </View>
     );
   }
