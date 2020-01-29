80713da4c5c27247e27a14bbb874ff60e7bf3a5b
Omar <omar.garciaf247@gmail.com>
2020-01-28 22:10:08 -0500

    landing now works with text and placeholders

--- components/Landing/Landing.tsx
+++ components/Landing/Landing.tsx
@@ -1,22 +1,55 @@
 import React from 'react';
-import { Text, View, TextInput, TouchableOpacity, Image } from 'react-native';
+import { Text, View, TextInput, TouchableOpacity, Image, TouchableHighlightBase } from 'react-native';
+import { LandingInterfaceProps, LandingInterfaceState } from '../../interfaces/Landing.interface';
 import landingStyles from './Landing.styles';
 import generalStyles from '../../styles/generalStyles';
 import landingLogo from '../../assets/img/app_dark_logo.png';
 import playButton from '../../assets/img/play.png';
 
-export default class Landing extends React.Component {
+const sampleQueries = [
+  "I'm falling in love",
+  "Just got to a party",
+  "My friend is getting married",
+  "Trying to study",
+]
+
+export default class Landing extends React.Component<LandingInterfaceProps, LandingInterfaceState> {
   constructor(props: object) {
     super(props);
     this.state = {
+      placeHolder: sampleQueries[0],
       query: '',
     };
+    this.changePlaceHolder = this.changePlaceHolder.bind(this);
   }
 
-  updateText = () => {
-    // this.setState({
-    //   query: 'random'
-    // });
+  componentDidMount() {
+    this.changePlaceHolder();
+  }
+
+  changePlaceHolder = () => {
+    const { query } = this.state;
+    let currPlaceHolder = 0;
+
+    const placeHolderTimer = setInterval(() => {
+      if (query !== '') {
+        clearInterval(placeHolderTimer);
+      }
+
+      if (currPlaceHolder > sampleQueries.length - 1) {
+        currPlaceHolder = 0;
+      }
+
+      this.setState({
+        placeHolder: sampleQueries[currPlaceHolder++],
+      })
+    }, 3000)
+  }
+
+  updateQuery = () => {
+    this.setState({
+      query: 'random'
+    });
   }
 
   checkVibe = () => {
@@ -24,13 +57,17 @@ export default class Landing extends React.Component {
   }
 
   render() {
+    const { placeHolder, query } = this.state;
+
     return (
       <View style={landingStyles.landing}>
         <View style={landingStyles.landingContainer}>
           <Text style={[generalStyles.title, landingStyles.elementMargin]}>What's the story?</Text>
-          <TextInput placeholder="I'm feeling lit" style={[generalStyles.queryText, landingStyles.elementMargin, landingStyles.queryBox]}>
+          <TextInput onChange={this.updateQuery} value={query} placeholderTextColor="rgba(0,0,0,0.3)" placeholder={placeHolder} style={[generalStyles.queryText, landingStyles.elementMargin, landingStyles.queryBox]}>
           </TextInput>
-          <Image style={[landingStyles.playButton, landingStyles.elementMargin]} source={playButton} />
+          <TouchableOpacity style={[landingStyles.playBox, landingStyles.elementMargin]}>
+            <Image style={landingStyles.playButton} source={playButton} />
+          </TouchableOpacity>
           <TouchableOpacity style={landingStyles.playlistButton}>
             <Text style={generalStyles.titleButton}>don't have one?</Text>
           </TouchableOpacity>
---
+++ interfaces/Landing.interface.tsx
@@ -0,0 +1,8 @@
+export interface LandingInterfaceProps {
+  // query: string,
+}
+
+export interface LandingInterfaceState {
+  placeHolder: string,
+  query: string,
+}
