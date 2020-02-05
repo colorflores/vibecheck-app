5c5d4894ff34f089820f9130c99a0cee244b70a1
Omar <omar.garciaf247@gmail.com>
2020-02-05 13:56:04 -0500

    added test util for storing asynch data to device

--- util/Storage.util.tsx
+++ util/Storage.util.tsx
@@ -1,3 +1,21 @@
 import { AsyncStorage } from 'react-native';
 
 // ?Saving tokens to the device
+export const saveData = async () => {
+  try {
+    await AsyncStorage.setItem('TEST_ITEM', 'check this out');
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export const getData = async () => {
+  try {
+
+  } catch (error) {
+    const value = await AsyncStorage.getItem('TEST_ITEM');
+    if (value) {
+      console.log(value);
+    }
+  }
+}
