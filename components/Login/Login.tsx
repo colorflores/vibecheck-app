6323a9acf4c097fe15f94d3d7a1e9c0c80a01427
Omar <omargarciaflores@cmail.carleton.ca>
2020-02-24 12:54:21 -0500

    update to login process

--- components/Login/Login.tsx
+++ components/Login/Login.tsx
@@ -42,27 +42,27 @@ export default class Login extends React.Component <LoginInterfaceProps, LoginIn
       }
     );
 
-    navigate('Loading');
+    // navigate('Loading');
 
-    // await wait(1000);
+    await wait(1000);
 
-    // if (expiryTime !== null) {
-    //   const newToken = await getData('ACCESS_TOKEN');
+    if (expiryTime !== null) {
+      const newToken = await getData('ACCESS_TOKEN');
         
-    //   initializeAPI(newToken);
-    //   navigate('Landing');
-    // } else {
-    //   Alert.alert(
-    //     'Vibecheck',
-    //     `There was an error logging in, the time retreieved = ${expiryTime}`,
-    //     [{
-    //       text: 'Done',
-    //       style: 'default'
-    //     }], { 
-    //       cancelable: true
-    //     }
-    //   );
-    // }
+      initializeAPI(newToken);
+      navigate('Landing');
+    } else {
+      Alert.alert(
+        'Vibecheck',
+        `There was an error logging in, the time retreieved = ${expiryTime}`,
+        [{
+          text: 'Done',
+          style: 'default'
+        }], { 
+          cancelable: true
+        }
+      );
+    }
   }
 
   render() {
