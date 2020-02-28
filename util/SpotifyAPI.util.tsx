a484b9d10d774a10ec2ace865379f903d1b83260
nathan embaye <nathanembaye@dhcp-140-148.cu-wireless-students.carleton.ca>
2020-02-28 18:06:36 -0500

    Saves playlist, provides alert to user when concluded.

--- util/SpotifyAPI.util.tsx
+++ util/SpotifyAPI.util.tsx
@@ -7,3 +7,16 @@ export const initializeAPI = (token: string) => {
   s.setAccessToken(token);
 }
 
+export const savePlaylist = async (nameOfPlaylist: string) => {
+
+  const playlistRequest = await fetch('https://api.spotify.com/v1/users/{user_id}/playlists',{
+    method: 'POST', 
+    headers: {
+      'Authorization': `Bearer ${s.getAccessToken()}`,   
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({name: nameOfPlaylist
+
+    })
+  })
+}
